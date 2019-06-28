7b4826c7e98bcc03db15b3cf50af7004865bcac7
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2019-06-28 10:36:39 -0700

    added link to KotlinCamp github

--- src/Components/Commons/Footer.js
+++ src/Components/Commons/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => (
         <li><a href="https://www.linkedin.com/company/19181085/" target="_blank" rel="noopener noreferrer"><i  className="fab fa-linkedin fa-2x"></i></a></li>
         <li><a href="https://www.facebook.com/kotlincamp/" target="_blank" rel="noopener noreferrer"><i  className="fab fa-facebook fa-2x"></i></a></li>
         <li><a href="https://kotlincamp.slack.com/" target="_blank" rel="noopener noreferrer"><i  className="fab fa-slack fa-2x"></i></a></li>
+        <li><a href="https://github.com/kotlincamp" target="_blank" rel="noopener noreferrer"><i  className="fab fa-github fa-2x"></i></a></li>
       </ul>
       <span>&copy; KotlinCamp. All rights reserved.</span>
     </div>
